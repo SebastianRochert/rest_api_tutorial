1b1cffad955a0c74d5cbcc22f56ac2a8853c6070
SebastianRochert <sebastian.rochert@t-online.de>
2021-11-03 09:08:11 +0100

    Added Log Timestamps and output port

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 //console.log("hello world from our API");
 import express from "express";
-import config from 'config'
+import config from 'config';
+import connect from "./utils/connect";
+import logger from "./utils/logger";
 
 const port = config.get<number>('port');
 
 const app = express();
 
-app.listen(port, () => { //Tutorial said 1337 but this address is already in use
-    console.log("App is running");
+app.listen(port, async() => { //Tutorial said 1337 but this address is already in use
+    //console.log("App is running");
+    logger.info(`App is running at http://localhost:${port }`);
+    await connect();
 })
